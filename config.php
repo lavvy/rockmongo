26912e715e032704e008cc2413fe4e565259d486
lavvy <lavashonline@gmail.com>
2016-09-20 01:57:17 -0400

    Update config.php

--- config.php
+++ config.php
@@ -20,8 +20,8 @@ $i = 0;
 */
 $MONGO["servers"][$i]["mongo_name"] = "mongo";//mongo server name
 //$MONGO["servers"][$i]["mongo_sock"] = "/var/run/mongo.sock";//mongo socket path (instead of host and port)
-$MONGO["servers"][$i]["mongo_host"] = "172.17.0.17";//mongo host
-$MONGO["servers"][$i]["mongo_port"] = "27017";//mongo port
+$MONGO["servers"][$i]["mongo_host"] = "212.47.245.243";//mongo host
+$MONGO["servers"][$i]["mongo_port"] = "28017";//mongo port
 $MONGO["servers"][$i]["mongo_timeout"] = 0;//mongo connection timeout
 //$MONGO["servers"][$i]["mongo_db"] = "MONGO_DATABASE";//default mongo db to connect, works only if mongo_auth=false
 //$MONGO["servers"][$i]["mongo_user"] = "MONGO_USERNAME";//mongo authentication user name, works only if mongo_auth=false
